40888cc8babeb48239bb8a47f955b990b74ac81d
Anton Oresten <antonoresten@gmail.com>
2025-09-01 13:31:18 +0200

    fix tests

--- test/Float8s/runtests.jl
+++ test/Float8s/runtests.jl
@@ -7,15 +7,15 @@ using Float8s: Float8, Float8_4
     @testset "E3M4" begin
 
         @testset for i in 0x00:0xfe
-            @test FP8_E3M4(Float32(reinterpret(Float8, i))) ≡ reinterpret(FP8_E3M4, i)
+            @test Float8_E3M4(Float32(reinterpret(Float8, i))) ≡ reinterpret(Float8_E3M4, i)
 
-            @test Float8(Float32(reinterpret(FP8_E3M4, i))) ≡ reinterpret(Float8, i)
+            @test Float8(Float32(reinterpret(Float8_E3M4, i))) ≡ reinterpret(Float8, i)
 
             @test Float32(reinterpret(Float8, i)) ≡
-                  Float32(reinterpret(FP8_E3M4, i))
+                  Float32(reinterpret(Float8_E3M4, i))
 
             @test Float32(Float8(Float32(reinterpret(Float8, i)))) ≡
-                  Float32(FP8_E3M4(Float32(reinterpret(FP8_E3M4, i))))
+                  Float32(Float8_E3M4(Float32(reinterpret(Float8_E3M4, i))))
         end
 
     end
@@ -23,15 +23,15 @@ using Float8s: Float8, Float8_4
     @testset "E4M3" begin
 
         @testset for i in 0x00:0xfe
-            @test FP8_E4M3(Float32(reinterpret(Float8_4, i))) ≡ reinterpret(FP8_E4M3, i)
+            @test Float8_E4M3(Float32(reinterpret(Float8_4, i))) ≡ reinterpret(Float8_E4M3, i)
 
-            @test Float8_4(Float32(reinterpret(FP8_E4M3, i))) ≡ reinterpret(Float8_4, i)
+            @test Float8_4(Float32(reinterpret(Float8_E4M3, i))) ≡ reinterpret(Float8_4, i)
 
             @test Float32(reinterpret(Float8_4, i)) ≡
-                  Float32(reinterpret(FP8_E4M3, i))
+                  Float32(reinterpret(Float8_E4M3, i))
 
             @test Float32(Float8_4(Float32(reinterpret(Float8_4, i)))) ≡
-                  Float32(FP8_E4M3(Float32(reinterpret(FP8_E4M3, i))))
+                  Float32(Float8_E4M3(Float32(reinterpret(Float8_E4M3, i))))
         end
 
     end
